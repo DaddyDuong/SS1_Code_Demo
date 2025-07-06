6aec5f76c8c59e79827cbfd02e34c2f55bcf7005
DaddyDuong <vxduong1504@gmail.com>
2025-07-06 23:50:37 +0700

    thêm plot

--- src/main/java/org/example/SolveRiverProblem.java
+++ src/main/java/org/example/SolveRiverProblem.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.moeaframework.algorithm.NSGAII;
+import org.moeaframework.analysis.plot.XYPlotBuilder;
 import org.moeaframework.problem.Problem;
 
 public class SolveRiverProblem {
@@ -17,5 +18,9 @@ public class SolveRiverProblem {
 
         // Hiển thị kết quả Pareto front
         algorithm.getResult().display();
+
+        new XYPlotBuilder()
+		    .scatter("NSGA-II", algorithm.getResult())
+		    .show();
     }
 }
